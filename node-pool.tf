1012f79403779f29f240adcd9bba2166132ae25e
Brad Sickles <brad.sickles@gmail.com>
2026-07-10 14:22:36 -0400

    Added compliance labels to node pool instances

--- node-pool.tf
+++ node-pool.tf
@@ -51,8 +51,8 @@ resource "google_container_node_pool" "this" {
     spot            = var.spot
     service_account = local.node_service_account_email
     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
-    labels          = local.labels
-    resource_labels = local.resource_labels
+    labels          = local.k8s_labels
+    resource_labels = local.labels
     tags            = ["gke-node", "${local.cluster_name}-gke"]
 
     disk_size_gb = var.disk_size
--- nullstone.tf
+++ nullstone.tf
@@ -10,16 +10,7 @@ resource "random_string" "resource_suffix" {
 }
 
 locals {
-  labels        = data.ns_workspace.this.gcp_labels
-  stack_name    = data.ns_workspace.this.stack_name
-  block_name    = data.ns_workspace.this.block_name
-  env_name      = data.ns_workspace.this.env_name
-  block_ref     = data.ns_workspace.this.block_ref
-  resource_name = "${local.block_ref}-${random_string.resource_suffix.result}"
-
-  resource_labels = {
-    nullstone-stack = local.stack_name
-    nullstone-block = local.block_name
-    nullstone-env   = local.env_name
-  }
+  labels     = data.ns_workspace.this.gcp_labels
+  k8s_labels = data.ns_workspace.this.k8s_labels
+  block_name = data.ns_workspace.this.block_name
 }
