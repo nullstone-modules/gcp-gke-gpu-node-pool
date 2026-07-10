99c83fe182f398925d68b25ca6cf09078a623a90
Brad Sickles <brad.sickles@gmail.com>
2026-07-10 15:08:36 -0400

    Don't fail if missing dataclassification

--- nullstone.tf
+++ nullstone.tf
@@ -19,7 +19,7 @@ locals {
     environment        = local.env_name
     owner              = local.labels["owner"]
     project            = local.stack_name
-    dataclassification = local.labels["dataclassification"]
+    dataclassification = try(local.labels["dataclassification"], null)
     application        = local.block_name
 
     "nullstone.io/env"   = local.env_name
